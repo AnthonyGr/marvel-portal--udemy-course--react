83bebadd1f7c868a9828df7a4b04945903217073
Anton <grishin.ant.a@gmail.com>
2021-12-07 15:59:15 +0500

    Добавлена загрузка новых персонажей по кнопке

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -8,31 +10,51 @@ class CharList extends Component {
   state = {
     chars: [],
     loading: true,
+    newItemsLoading: true,
     error: false,
+    offset: 600,
+    charEnded: false,
   };
 
   componentDidMount() {
-    this.updateChars();
+    this.onCharListLoading();
+    this.onRequest();
   }
 
   marvelService = new MarvelService();
 
-  onCharsLoaded = (chars) => {
-    this.setState({ chars, loading: false });
+  onCharsLoaded = (newChars) => {
+    let ended = false;
+    if (newChars.length < 9) {
+      ended = true;
+    }
+
+    this.setState(({ offset, chars }) => ({
+      chars: [...chars, ...newChars],
+      loading: false,
+      newItemsLoading: false,
+      offset: offset + 9,
+      charEnded: ended,
+    }));
+  };
+
+  onRequest = (offset) => {
+    this.onCharListLoading();
+    this.marvelService.getAllCharacters(offset).then(this.onCharsLoaded).catch(this.onError);
+  };
+
+  onCharListLoading = () => {
+    this.setState({ newItemsLoading: true });
   };
 
   onError = () => {
     this.setState({ error: true, lodaing: false });
   };
 
-  updateChars = () => {
-    this.marvelService.getAllCharacters().then(this.onCharsLoaded).catch(this.onError);
-  };
-
   renderItems = (chars) => {
     const imageNotFound = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg';
     return chars.map((char) => (
-      <li className="char__item char__item_selected" key={char.id} onClick={() => this.props.onCharSelected(char.id)}>
+      <li className="char__item" key={char.id} onClick={() => this.props.onCharSelected(char.id)}>
         <img
           src={char.thumbnail}
           alt="abyss"
@@ -44,7 +66,7 @@ class CharList extends Component {
   };
 
   render() {
-    const { loading, error } = this.state;
+    const { loading, error, offset, newItemsLoading, charEnded } = this.state;
     const errorMessage = error ? <ErrorMessage /> : null;
     const spinner = loading ? <Spinner /> : null;
     const content = !(error || spinner) ? this.renderItems(this.state.chars) : null;
@@ -54,12 +76,21 @@ class CharList extends Component {
         {errorMessage}
         {spinner}
         <ul className="char__grid">{content}</ul>
-        <button className="button button__main button__long">
+        <button
+          className="button button__main button__long"
+          disabled={newItemsLoading}
+          style={{ display: charEnded ? 'none' : 'block' }}
+          onClick={() => this.onRequest(offset)}
+        >
           <div className="inner">load more</div>
         </button>
       </div>
     );
   }
 }
+
+CharList.propTypes = {
+  onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=dae97b66d2439a8ab63d6fc9f354e60d';
+  _baseOffset = 150;
 
   getResoure = async (url) => {
     const res = await fetch(url);
@@ -10,8 +11,8 @@ class MarvelService {
     return res.json();
   };
 
-  getAllCharacters = async () => {
-    const res = await this.getResoure(`${this._apiBase}characters?limit=9&offset=50&${this._apiKey}`);
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getResoure(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
     return res.data.results.map(this._transformCharecter);
   };
 

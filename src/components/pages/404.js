bf7923d8fe0dfbd8fa054e6ff8d5f1b0021cd51c
Anton <grishin.ant.a@gmail.com>
2022-01-05 16:33:40 +0500

    поправлен title на странице 404

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 const Page404 = () => {
   return (
     <div>
+      <Helmet>
+        <meta name="description" content="Error 404 - page not found" />
+        <title>404 Page Not Found</title>
+      </Helmet>
       <ErrorMessage />
       <p style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '24px' }}>404 - Page Not Found</p>
       <Link
